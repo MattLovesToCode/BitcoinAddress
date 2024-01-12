1811c2be4742a4820f525483b8daffc2a1acbe2b
Matthew <134560399+MattLovesToCode@users.noreply.github.com>
2024-01-12 11:34:20 -0600

    Update Bitcoin.py

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,13 +1,9 @@
-This is a simple Bitcoin Wallet Address Project I used from a video I watched
+This is a simple Bitcoin Wallet Address Project
 
-# import bitcoin
 from bitcoin import random_key, privtopub, pubtoaddr
-# Create a Private Key
 private_key = random_key()
 print("Private Key:", private_key)
-# Create Public Key
 public_key = privtopub(private_key)
 print("Public Key:", public_key)
-# Create A Bitcoin Address
 address = pubtoaddr(public_key)
 print("My Address is:", address)
